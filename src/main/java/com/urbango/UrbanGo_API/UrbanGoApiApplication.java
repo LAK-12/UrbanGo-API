57de9ea95ced09701159d9c201aad6d727b924d3
Lamees <lamees.amin@gmail.com>
2025-11-02 22:24:10 -0500

    Initialized SpringBoot project

---
+++ src/main/java/com/urbango/UrbanGo_API/UrbanGoApiApplication.java
@@ -0,0 +1,13 @@
+package com.urbango.UrbanGo_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanGoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanGoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbango/UrbanGo_API/UrbanGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbango.UrbanGo_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanGoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
